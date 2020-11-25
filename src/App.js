917b5be2cacaf421c7df49bcfd3362ae32033cad
andrewreardigan <andrewreardigan@icloud.com>
2020-11-25 09:09:08 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Map from './components/Map';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ eventData, setEventData ] = useState([]);
+	const [ loading, setLoading ] = useState(false);
+
+	useEffect(() => {
+		const getEvents = async () => {
+			setLoading(true);
+			const res = await axios.get('https');
+			const { events } = await axios.get('', res.json());
+
+			setEventData(events);
+			setLoading(false);
+		};
+	});
+
+	return (
+		<div className="App">
+			<Map />
+		</div>
+	);
 }
 
 export default App;
